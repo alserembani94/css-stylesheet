261f9b747f3b0c35f913a1fc3a190fac533c0674
Atif Aiman <alserembani94@gmail.com>
2023-08-01 22:31:57 +0800

    add stylesheet

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import HelloReact from "./components/HelloReact";
+
+function App() {
+  return (
+    <>
+      <HelloReact />
+      <p className="title">Hello React</p>
+      <p className="title">Hello React</p>
+      <p className="title">Hello React</p>
+      <p className="title">Hello React</p>
+      <p className="title">Hello React</p>
+      <p className="title">Hello React</p>
+      <p className="title">Hello React</p>
+      <p className="title">Hello React</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/HelloReact.jsx
@@ -0,0 +1,7 @@
+import "./custom.css";
+
+const HelloReact = () => {
+  return <p className="title">Hello React!</p>;
+};
+
+export default HelloReact;
